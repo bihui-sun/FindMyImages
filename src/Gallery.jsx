600a15d29363af1b618a62ea76f48fbb07207551
Bihui Sun <bihui.sun1107@gmail.com>
2025-02-08 15:51:03 +1300

    api key deleted before put in env file

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -2,8 +2,7 @@ import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 import { useGlobalContext } from './context'
 
-const url =
-  'https://api.unsplash.com/search/photos?client_id=LT3LlCotzbVNZ6vPZl5aIhGl3TTIX545ucAbw7vG3HI&query='
+const url = 'https://api.unsplash.com/search/photos?client_id=API_KEY&query='
 
 const Gallery = () => {
   const { searchTerm } = useGlobalContext()
